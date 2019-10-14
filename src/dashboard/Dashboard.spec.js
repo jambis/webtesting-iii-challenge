7ebaf06fe9cc40b833b8476b6b8db1c90de0ae18
jambis <imjaamees@gmail.com>
2019-10-14 16:38:06 -0500

    dashboard tests

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,18 @@
-// Test away
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import Dashboard from "./Dashboard";
+
+afterEach(cleanup);
+
+describe("Dashboard component", () => {
+  it("renders without crashing", () => {
+    render(<Dashboard />);
+  });
+
+  it("Default state is open and unlocked", () => {
+    const { queryByText } = render(<Dashboard />);
+
+    expect(queryByText(/open/i)).toBeTruthy();
+    expect(queryByText(/unlocked/i)).toBeTruthy();
+  });
+});
